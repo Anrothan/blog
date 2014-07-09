96618dce742046d51818a277518bd9b6801a1d7e
Derek Maffett <derekgmaffett@gmail.com>
2014-07-09 13:09:08 -0700

    Add user authentication/authorization and css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,62 +1,69 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate, :except => [:index, :show]
+  before_action :set_article, only: :show
+   
   # GET /articles
   # GET /articles.json
   def index
     @articles = Article.all
   end
-
+   
   # GET /articles/1
   # GET /articles/1.json
   def show
   end
-
+   
   # GET /articles/new
+  # GET /articles/new.json
   def new
     @article = Article.new
   end
-
+   
   # GET /articles/1/edit
   def edit
+    @article = current_user.articles.find(params[:id])
   end
-
+   
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
-
+    @article = current_user.articles.new(article_params)
+     
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
+        format.html { redirect_to(@article, :notice => 'Article was successfully created.') }
+        format.json { render action: 'show', :status => :created, :location => @article }
       else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.json { render json: @article.errors, :status => :unprocessable_entity }
       end
     end
   end
-
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
+   
+  # PUT /articles/1
+  # PUT /articles/1.json
   def update
+    @article = current_user.articles.find(params[:id])
+   
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
+        format.html { redirect_to(@article, :notice => 'Article was successfully updated.') }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.json { render :json => @article.errors, :status => :unprocessable_entity }
       end
     end
   end
-
+   
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
+    @article = current_user.articles.find(params[:id])
     @article.destroy
+     
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.html { redirect_to(articles_url) }
       format.json { head :no_content }
     end
   end
@@ -66,9 +73,10 @@ class ArticlesController < ApplicationController
     def set_article
       @article = Article.find(params[:id])
     end
-
+     
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :location, :excerpt, :body, :published_at)
+      params.require(:article).permit(:title, :location, :excerpt, :body, :published_at,
+      :category_ids => [])
     end
 end
